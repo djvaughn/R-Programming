089e888963328aa4bc6cf9b0cd6248439fcfe5cc
Daniel Vaughn <djvaughn@email.arizona.edu>
2017-02-14 17:56:31 -0600

    Finished section 2

---
+++ RScripts/Section 2/LogicalVariablesandOperators.R
@@ -0,0 +1,26 @@
+#Logical:
+#TRUE T
+#FALSE F
+
+4 < 5
+10 > 100
+4 == 5
+# ==
+# !=
+# <
+# >
+# <=
+# >=
+# !
+# |
+# &
+# isTRUE(x)
+
+result <- 4 < 5
+isTRUE(result)
+
+result2 <- !(5 > 1)
+result2
+
+result | result2
+result & result2
---
+++ RScripts/Section 2/TypesOfVariables.R
@@ -0,0 +1,28 @@
+
+
+#integer
+x <- 2L
+typeof(x)
+
+#double
+y <- 2.5
+typeof(y)
+
+#The L makes sure the number is an int.
+#If not it will store it as a double
+x <- 2
+typeof(x)
+
+#complex
+z <- 3 + 2i
+typeof(z)
+
+#character
+a <- "h"
+typeof(a)
+
+#logical
+q1 <- T
+typeof(q1)
+q2 <- FALSE
+typeof(q2)
